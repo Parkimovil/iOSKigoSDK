3588191786d44cc23add5b16452fea9cc20255c9
Javier Alex Gonzalez <javalegonzalez@gmail.com>
2024-04-23 12:00:48 -0600

    Package and Framework files

---
+++ Sources/KigoFramework.xcframework/ios-arm64/KigoFramework.framework/Headers/KigoFramework.h
@@ -0,0 +1,18 @@
+//
+//  KigoFramework.h
+//  KigoFramework
+//
+//  Created by Javier Alex Gonzalez on 22/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KigoFramework.
+FOUNDATION_EXPORT double KigoFrameworkVersionNumber;
+
+//! Project version string for KigoFramework.
+FOUNDATION_EXPORT const unsigned char KigoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KigoFramework/PublicHeader.h>
+
+
